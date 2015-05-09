4b2cdd93c95644e509ea10b8877b4f6730ffed7c
Shweta <Shweta@Shwetas-MacBook-Pro.local>
2015-05-09 15:39:58 -0400

    working server

--- videomap/videomap/settings.py
+++ videomap/videomap/settings.py
@@ -14,6 +14,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+Temp_Path = os.path.realpath('.')
 
 
 # Quick-start development settings - unsuitable for production
@@ -69,6 +70,11 @@ TEMPLATES = [
     },
 ]
 
+TEMPLATE_DIRS = (
+    Temp_Path +"/template",
+    os.path.join(Temp_Path, 'templates'),
+)
+
 WSGI_APPLICATION = 'videomap.wsgi.application'
 
 
@@ -101,3 +107,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATIC_ROOT = ''    
+# URL prefix for static files.
+# Example: "http://example.com/static/", "http://static.example.com/"
+
+
